94d33b95696eddcb22a0fbf3ff375debed9d1953
Dima Ludov <lDummyl>
2021-08-09 21:13:36 +0300

    git all

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,7 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 
+
 	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
