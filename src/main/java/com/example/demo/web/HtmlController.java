d1a55520c89902f9500236a3fba1e20874b011e9
Dima Ludov <lDummyl>
2021-08-09 21:24:18 +0300

    add log

--- src/main/java/com/example/demo/web/HtmlController.java
+++ src/main/java/com/example/demo/web/HtmlController.java
@@ -12,6 +12,7 @@ public class HtmlController {
 
     @GetMapping( "/hello/{name}")
     public String hello(Model model, @PathVariable String name) {
+        log.debug(name);
         model.addAttribute("name", name);
         return "hello";
     }
