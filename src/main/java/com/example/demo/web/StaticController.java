156ef57a237e2e9bc0da208d7ac8c6c092eb6336
Sergey Zhak <s.zhak@semrush.com>
2021-08-12 19:08:25 +0300

    Unnecessary field removed

--- src/main/java/com/example/demo/web/StaticController.java
+++ src/main/java/com/example/demo/web/StaticController.java
@@ -29,7 +29,7 @@ public class StaticController {
     private final DriverRepository driverRepository;
 
     @GetMapping("/index")
-    public String index(Model model) {
+    public String index() {
         return "index";
     }
 
